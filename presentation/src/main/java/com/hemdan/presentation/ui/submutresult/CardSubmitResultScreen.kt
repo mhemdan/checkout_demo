bc127ec78cfdae29fdbc98b01b9f9fa3692b7b33
mhemdan <mohammed.hemdan.faraj@gmail.com>
2022-10-08 20:30:19 +0200

    [ADD] add success & failure text on the result screen

--- presentation/src/main/java/com/hemdan/presentation/ui/submutresult/CardSubmitResultScreen.kt
+++ presentation/src/main/java/com/hemdan/presentation/ui/submutresult/CardSubmitResultScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.hemdan.presentation.R
 
 @Composable
@@ -21,6 +24,11 @@ fun CardSubmitResultScreen(isSubmittedSuccessfully: Boolean) {
         Image(
             painter = painterResource(id = if (isSubmittedSuccessfully) R.drawable.success else R.drawable.failure),
             contentDescription = null
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = stringResource(id = if (isSubmittedSuccessfully) R.string.app_payment_success else R.string.app_payment_failed)
         )
     }
 }
